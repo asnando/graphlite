a024a1c4c3568af5b52f9f77a3fc0b5f510452b2
Fernando Rama <fernandorama.dev@gmail.com>
2019-03-22 15:55:18 -0300

    Update webpack distribution

--- webpack.config.js
+++ webpack.config.js
@@ -4,65 +4,33 @@ const webpack = require('webpack');
 const MODE = 'production';
 const DEVTOOL = 'source-map';
 
-const defaultPlugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      WEBPACK_ENV: true,
-    }
-  }),
-];
-
-const defaultConfig = {
-  entry: './src/index.js',
-  output: {
-    filename: 'graphlite.min.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  mode: MODE,
-  devtool: DEVTOOL,
-  module: {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: ['babel-loader']
-    }
-  ]
-  }
-};
-
-const webBundle = Object.assign({}, defaultConfig, {
+module.exports = {
+  entry: "./src/index.js",
   output: {
     filename: "graphlite.js",
+    path: path.resolve(__dirname, "dist"),
     library: "graphlite",
-    libraryTarget: "umd",
-    path: path.join(__dirname, '/dist'),
+    libraryTarget: "umd"
   },
   mode: MODE,
   devtool: DEVTOOL,
-  entry: {
-    main: './src/index.js'
-  },
-  plugins: defaultPlugins
-});
-
-const reactNativeBundle = Object.assign({}, defaultConfig, {
-  entry: './src/index.js',
   externals: {
     'react-native': 'react-native'
   },
-  mode: MODE,
-  devtool: DEVTOOL,
-  output: {
-    filename: "graphlite.js",
-    library: "graphlite",
-    libraryTarget: "umd",
-    path: path.join(__dirname, '/dist/react-native'),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ],
   },
-  plugins: defaultPlugins
-});
-
-module.exports = [
-  webBundle,
-  reactNativeBundle
-];
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        WEBPACK_ENV: true,
+      }
+    }),
+  ]
+};
