b555744ab41a84a5e04e4cbd2f26950eb8ad89ed
Fernando Rama <fernandorama.dev@gmail.com>
2019-03-22 11:57:50 -0300

    Bug Fix: Remove limit option from root collection (issue #3.1)

--- src/resolvers/main.js
+++ src/resolvers/main.js
@@ -9,6 +9,12 @@ module.exports = function graphNodeResolver(node, options = {}, nextNodes, custo
     ? `select json_patch(json_object($fields_as_json), ($next_nodes)) $object_name from (select $raw_fields $source $show_options $options) $table_alias`
     : `(select json_object($node_name, (select json_group_array(json_patch(json_object($fields_as_json), ($next_nodes))) from (select $raw_fields $source $show_options $options) $table_alias)))`;
 
+  let nodeOptions = node.getOptions(options, ['group', 'order', 'limit', 'offset']);
+
+  if (!node.parentAssociation) {
+    nodeOptions = nodeOptions.replace(/LIMIT\s\d+/, '');
+  }
+
   let query = struct
       .replace(/\$table_alias/,     node.getTableAlias())
       .replace(/\$raw_fields/,      node.getRawFields())
@@ -18,9 +24,7 @@ module.exports = function graphNodeResolver(node, options = {}, nextNodes, custo
       .replace(/\$node_name/,       node.getAssociationName())
       .replace(/\$next_nodes/,      nextNodes(options))
       .replace(/\$show_options/,    node.getShowOptions(options))
-      .replace(/\$options/,         node.getOptions(options, ['group', 'order', 'limit', 'offset']))
-
-  // debug.debug(node.getOptions(options, ['group', 'order', 'limit', 'offset']));
+      .replace(/\$options/,         nodeOptions)
 
   // Build the filter subquery in order to select the root schema
   // identifiers that will be returned by the select.
