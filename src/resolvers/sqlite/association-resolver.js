73bf5021f6cf67707dbcc577af0ee56f821926e9
Fernando Rama <fernandorama.dev@gmail.com>
2019-05-28 11:09:10 -0300

    split association and source resolver into files

---
+++ src/resolvers/sqlite/association-resolver.js
@@ -0,0 +1,52 @@
+const SQLiteAssociationResolver = (schema, options, node, resolveNextNodes) => {
+  if (node.isRoot()) return '';
+
+  const parentSchema = node.parent.getValue();
+  const parentSchemaName = parentSchema.getSchemaName();
+  const resolvedAssociation = schema.getAssociationWith(parentSchemaName);
+
+  // Nested nodes can have another tables(fk) between associations. In that case,
+  // we need to use the "using" middleware tables array to reach the associated data.
+  // All the associations are translated to an array of table associations.
+  const associationList = ([resolvedAssociation]).concat(resolvedAssociation.using);
+
+  return associationList
+    // Ignore "left" join(s) and foreign tables that came from it.
+    .filter((association, index, self) => !/left/i.test(!index ? association.joinType : self[0].joinType))
+    .map(({
+      sourceHash,
+      targetTable,
+      targetKey,
+      targetHash,
+      foreignTable,
+      foreignKey,
+      useSourceKey,
+      useTargetKey,
+      joinType,
+    }, index, self) => {
+      if (foreignTable && foreignKey) {
+        // Fix: When using foreign table it must update the last position of the array as it
+        // contains the final association with the desired table. If does not change it will try
+        // to join the two tables ignoring everything in the middle.
+        // eslint-disable-next-line no-param-reassign
+        self[self.length - 1] = {
+          ...self[self.length - 1],
+          sourceTable: targetTable,
+          sourceHash: targetHash,
+        };
+        // When foreign table is defined it must join the table multiple times.
+        return `
+          ${joinType.toUpperCase()} JOIN ${foreignTable}
+            ON ${foreignTable}.${foreignKey}=${sourceHash}.${foreignKey}
+          ${joinType.toUpperCase()} JOIN ${targetTable} ${targetHash}
+            ON ${targetHash}.${useTargetKey || targetKey}=${foreignTable}.${useTargetKey || targetKey}
+        `;
+      }
+      return `
+        ${joinType.toUpperCase()} JOIN ${targetTable} ${targetHash}
+          ON ${targetHash}.${useTargetKey || targetKey}=${sourceHash}.${useSourceKey || targetKey}
+      `;
+    }).join(' ');
+};
+
+module.exports = SQLiteAssociationResolver;
--- src/resolvers/sqlite/source-with-associations-resolver.js
+++ src/resolvers/sqlite/source-with-associations-resolver.js
@@ -1,65 +1,11 @@
 const debug = require('../../debug');
+const resolveAssociation = require('./association-resolver');
+const resolveSource = require('./source-resolver');
 
 // todo: add description
-const SQLiteGraphNodeSourceWithAssociationsResolver = (
-  schema,
-  options,
-  node,
-  resolveNextNodes,
-) => {
-  const tableName = schema.getTableName();
-  const tableAlias = schema.getTableHash();
-  const parentSchemaName = node.root ? null : node.parent.getValue().getSchemaName();
-
-  if (!node.root) {
-    // ignore join(s) without filter value.
-    if (!schema.hasAssociatedOption(options)) return '';
-    const resolvedAssociation = schema.getAssociationWith(parentSchemaName);
-    // Nested nodes can have another schemas between associations. In that case,
-    // we need to use the "using" middleware associations array to reach the associated data.
-    return (resolvedAssociation.using || []).concat(resolvedAssociation)
-      // ignore left joins and middleware schemas which root association has left join too.
-      .filter((association, index, self) => (!index
-        ? !/left/i.test(association.joinType)
-        : !/left/i.test(self[0].joinType)))
-      .map(({
-        sourceTable,
-        sourceHash,
-        targetTable,
-        targetKey,
-        targetHash,
-        foreignTable,
-        foreignKey,
-        useSourceKey,
-        useTargetKey,
-        joinType,
-      }, index, self) => {
-        if (foreignTable && foreignKey) {
-          // Fix: When using foreign table it must update the last position of the array as it
-          // contains the final association with the desired table. If does not change it will try
-          // to join the two tables ignoring everything in the middle.
-          // eslint-disable-next-line no-param-reassign
-          self[self.length - 1] = {
-            ...self[self.length - 1],
-            sourceTable: targetTable,
-            sourceHash: targetHash,
-          };
-          // When foreign table is defined it must join the table multiple times.
-          return `
-            ${joinType.toUpperCase()} JOIN ${foreignTable}
-              ON ${foreignTable}.${foreignKey}=${sourceHash}.${foreignKey}
-            ${joinType.toUpperCase()} JOIN ${targetTable} ${targetHash}
-              ON ${targetHash}.${useTargetKey || targetKey}=${foreignTable}.${useTargetKey || targetKey}
-          `;
-        }
-        return `
-          ${joinType.toUpperCase()} JOIN ${targetTable} ${targetHash}
-            ON ${targetHash}.${useTargetKey || targetKey}=${sourceHash}.${useSourceKey || targetKey}
-        `;
-      }).join(' ');
-  }
-  // Resolves root node(source).
-  return `FROM ${tableName} ${tableAlias} ${resolveNextNodes()}`;
+const SQLiteGraphNodeSourceWithAssociationsResolver = (schema, options, node, resolveNextNodes) => {
+  const args = [schema, options, node, resolveNextNodes];
+  return node.isRoot() ? resolveSource(...args) : resolveAssociation(...args);
 };
 
 module.exports = SQLiteGraphNodeSourceWithAssociationsResolver;
