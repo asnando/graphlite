c623997903979908bfec75b2461f1fc91156cf3f
Fernando Rama <fernandorama.dev@gmail.com>
2019-05-28 17:49:49 -0300

    Prevent generated hash code begin with digit

--- src/utils/hash-code.js
+++ src/utils/hash-code.js
@@ -1,5 +1,7 @@
-const hashCode = () => (
-  Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
-);
+const hashCode = () => {
+  const hash = Math.random().toString(36).substring(2, 15)
+    + Math.random().toString(36).substring(2, 15);
+  return /^\d/.test(hash) ? hashCode() : hash;
+};
 
 module.exports = hashCode;
