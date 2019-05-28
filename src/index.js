5af10addd419096cad30b3c70a167ffb0d3d34be
Fernando Rama <fernandorama.dev@gmail.com>
2019-05-28 15:13:54 -0300

    Add simple query executer

--- src/index.js
+++ src/index.js
@@ -8,6 +8,20 @@ const schemaList = require('./jar/schema-list');
 const queryList = require('./jar/query-list');
 const formatQuery = require('./utils/query');
 
+const constants = require('./constants');
+
+const {
+  RESPONSE_OBJECT_NAME,
+  GRAPHLITE_CONNECTION_EXECUTER_NAME,
+} = constants;
+
+const parseDatabaseReponse = (rows) => {
+  rows = rows.map(row => JSON.parse(row[RESPONSE_OBJECT_NAME]));
+  return {
+    rows,
+  };
+}
+
 class GraphLite {
   constructor({
     schemas,
@@ -15,10 +29,12 @@ class GraphLite {
     associations,
     locales,
     defaultLanguage,
+    connection,
   }) {
     const supportedLocales = isObject(locales) ? keys(locales) : {};
 
     assign(this, {
+      connection,
       schemaList,
       queryList,
       supportedLocales,
@@ -50,16 +66,6 @@ class GraphLite {
     return this.queryList.getQuery(queryName);
   }
 
-  // Public API
-  findOne(...args) {
-    // assign(extraOptions, 'size', 1);
-    return this._mountQuery(...args);
-  }
-
-  findAll(...args) {
-    return this._mountQuery(...args);
-  }
-
   _mountQuery(queryName, options = {}, extraOptions = {}) {
     const query = this._getQuery(queryName);
     // !
@@ -68,7 +74,30 @@ class GraphLite {
     const resolvedQuery = formatQuery(query.resolve(mergedOptions));
     // debug.log(resolvedQuery);
     pbcopy.writeSync(resolvedQuery);
-    return Promise.resolve({});
+    return resolvedQuery;
+  }
+
+  _executeQuery(query) {
+    const { connection } = this;
+    if (isFunction(connection)) {
+      return connection(query);
+    }
+    return connection[GRAPHLITE_CONNECTION_EXECUTER_NAME](query);
+  }
+
+  _run(queryName, options = {}, extraOptions = {}) {
+    const query = this._mountQuery(queryName, options, extraOptions);
+    return this._executeQuery(query).then(parseDatabaseReponse);
+  }
+
+  // Public API
+  findOne(...args) {
+    // assign(extraOptions, 'size', 1);
+    return this._run(...args);
+  }
+
+  findAll(...args) {
+    return this._run(...args);
   }
 
   setLocale(locale) {
