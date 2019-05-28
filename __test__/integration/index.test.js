ea6c8677f826d625b70cd538bc23609637e64dbb
Fernando Rama <fernandorama.dev@gmail.com>
2019-05-28 14:37:06 -0300

    Initial index test file spec

--- __test__/integration/index.test.js
+++ __test__/integration/index.test.js
@@ -1,3 +1,32 @@
+const graphlite = require('../../src');
+const sqlite = require('sqlite-storage');
+const path = require('path');
+
+const databasePath = path.resolve(__dirname, '..', 'databases');
+const databases = [
+  {
+    name: 'data',
+    path: path.join(databasePath, 'test.db'),
+  },
+];
+
+let db = null;
+
+const graph = new graphlite({
+
+});
+
+beforeAll(async () => {
+  db = new sqlite({
+    databases,
+  });
+  await db.connect();
+});
+
+afterAll(async () => {
+  await db.close();
+});
+
 describe('graphlite', () => {
   it('should return 2 when 1 + 1', () => {
     const value = sum(1,1);
