7e7d1dddc0ac07ef4b33af0518d19250ada54c24
Fernando Rama <fernandorama.dev@gmail.com>
2019-05-28 15:13:37 -0300

    Update test files

--- __test__/integration/index.test.js
+++ __test__/integration/index.test.js
@@ -2,6 +2,12 @@ const graphlite = require('../../src');
 const sqlite = require('sqlite-storage');
 const path = require('path');
 
+let db, graph;
+
+const schema = (schemaName) => require(path.resolve(__dirname, '..', 'schemas', schemaName));
+const query = (queryName) => require(path.resolve(__dirname, '..', 'queries', queryName));
+const useAssociations = require('../useAssociations');
+
 const databasePath = path.resolve(__dirname, '..', 'databases');
 const databases = [
   {
@@ -10,16 +16,47 @@ const databases = [
   },
 ];
 
-let db = null;
+const defaultLanguage = 'pt-br';
+const locales = {
+  'pt-br': {
+    usePreffix: null,
+  },
+  en: {
+    usePreffix: 'E',
+  },
+};
 
-const graph = new graphlite({
+const schemas = [
+  schema('aplicacao'),
+  schema('fabricante'),
+  schema('grupo'),
+  schema('imagem'),
+  schema('montadora'),
+  schema('produto'),
+  schema('referencia'),
+  schema('subgrupo'),
+  schema('linhaProduto'),
+];
 
-});
+const queries = [
+  query('produto'),
+];
 
 beforeAll(async () => {
+  // create database instances
   db = new sqlite({
     databases,
   });
+  // create graphlite instance
+  graph = new graphlite({
+    connection: db.executeQuery.bind(db),
+    locales,
+    defaultLanguage,
+    schemas,
+    queries,
+    associations: useAssociations,
+  });
+  // connect databases list
   await db.connect();
 });
 
@@ -28,10 +65,9 @@ afterAll(async () => {
 });
 
 describe('graphlite', () => {
-  it('should return 2 when 1 + 1', () => {
-    const value = sum(1,1);
-    expect(value).toBe(2);
+  it('should return all products from database', async () => {
+    const data = await graph.findAll('produto');
+    const rows = data.rows;
+    expect(rows.length).toBe(6674);
   });
 });
-
-const sum = (a, b) => (a + b);
