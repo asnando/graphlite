4599ec033e28a49c4e6ce0ac7727128df85a8814
Fernando Rama <fernandorama.dev@gmail.com>
2019-06-03 16:57:37 -0300

    Fix: Root options resolver replacer fix

--- src/resolvers/sqlite/root-options.js
+++ src/resolvers/sqlite/root-options.js
@@ -11,7 +11,7 @@ const SQLiteGraphNodeRootOptionsResolver = (schema, options, node, resolveNextNo
     .filter(sql => sql && !/^\s{0,}$/.test(sql))
     // Resolve the begin of strings that is not the first from array replacing the 'where'
     // clause with 'AND'. This resolves the redudants WHERE which may cause the query to crash.
-    .map((sql, index) => (!index ? sql : sql.replace(/^where/i, ' AND')))
+    .map((sql, index) => (!index ? sql : sql.replace(/^\s{0,}where/i, ' AND')))
     .join('');
 };
 
