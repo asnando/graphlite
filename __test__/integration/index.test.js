29af30a2bb1855cfc6a955f668630a3976f64308
Fernando Rama <fernandorama.dev@gmail.com>
2019-06-03 18:12:12 -0300

    Add full integration test script

--- __test__/integration/index.test.js
+++ __test__/integration/index.test.js
@@ -29,56 +29,63 @@ afterAll(async () => {
   await db.close();
 });
 
+const findOne = async(queryName, options = {}) => {
+  const data = await graph.findOne(queryName, options);
+  return data;
+};
+
+const findAll = async(queryName, options = {}) => {
+  const data = await graph.findAll(queryName, options);
+  return data;
+};
+
 describe('graphlite', () => {
-
-  // it('should return 100 rows from database table ordered by description', async() => {
-  //   const options = { size: 100, page: 1 };
-  //   const data = await graph.findAll('lista-produtos', options);
-  //   const { count, rows } = data;
-  //   const [product] = rows;
-  //   console.log(product);
-  //   expect(count).toBe(100);
-  // });
-
-  // it('should return one product marked as release', async() => {
-  //   const options = {
-  //     isLancamento: true,
-  //     numeroProduto: 75260,
-  //   };
-  //   const data = await graph.findAll('lista-produtos', options);
-  //   const { rows } = data;
-  //   const [product] = rows;
-  //   console.log(product);
-  //   expect(product.isLancamento).toBe(true);
-  // });
-
-  it('should return one product with "FIORINO" vehicle associated', async() => {
+  it('should return a list with the first 30 products ordered by description', async() => {
     const options = {
-      // aplicacao: 'FIORINO',
-      idAplicacao: 50091,
+      size: 30,
+      page: 1,
     };
-    const data = await graph.findAll('lista-produtos', options);
-    const { rows } = data;
-    const [product] = rows;
-    console.log(product);
-    expect(typeof product).toBe('object');
+    const data = await findAll('lista-produtos', options);
+    const { count } = data;
+    expect(count).toBe(30);
   });
 
-  // it('should return one product with the 10785 id', async() => {
-  //   const options = { id: 10785 };
-  //   const data = await graph.findAll('produto', options);
-  //   const { rows } = data;
-  //   const [product] = rows;
-  //   expect(product.id).toBe(10785);
-  // });
+  it('should return a list with all products groups ordered by order column', async() => {
+    const data = await findAll('grupos');
+    const { rows } = data;
+    const [firstGroup] = rows;
+    expect(firstGroup.descricaoGrupo).toBe('JUNTAS');
+  });
 
-  // it('should return a product with picture', async() => {
-  //   const options = { id: 2291 };
-  //   const data = await graph.findAll('produto', options);
-  //   const { rows } = data;
-  //   const [product] = rows;
-  //   console.log(product);
-  //   expect(typeof product.imagemProduto).toBe('string');
-  // });
+  it('should return a list of products lines ordered by order column', async() => {
+    const data = await findAll('linhas-produtos');
+    const { rows } = data;
+    const [firstLine] = rows;
+    expect(firstLine.descricaoLinhaProduto).toBe('LINHA LEVE');
+  });
+
+  it('should return a list with automakers ordered by the order column', async() => {
+    const data = await findAll('montadoras');
+    const { rows } = data;
+    const [firstAutomaker] = rows;
+    expect(firstAutomaker.descricaoMontadora).toBe('CHEVROLET');
+  });
+
+  it('should return a list of vehicle of the "FIAT" automaker', async() => {
+    const data = await findAll('veiculos');
+    const { rows } = data;
+    const [firstVehicle] = rows;
+    expect(firstVehicle.descricaoAplicacao).toBe('100');
+  });
+
+  it('should return information about the product with "10694" id', async() => {
+    const options = {
+      id: 10694,
+    };
+    const data = await findOne('produto', options);
+    const { rows } = data;
+    const [product] = rows;
+    expect(product.id).toBe(10694);
+  });
 
 });
